fb8b9e80e85524e70d811fde22fe59024a0a36c0
vulkan-ops <vulkanops@gmail.com>
2021-03-03 15:57:43 -0300

    ocean: bringup for our project

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product-if-exists, vendor/motorola/ocean/ocean-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-aospk
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
