e9c711e0e2ea3fecd601c511277115890767aa0f
vulkan-ops <vulkanops@gmail.com>
2021-04-27 17:46:10 -0300

    ocean: rename path for kraken
    Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- device.mk
+++ device.mk
@@ -23,7 +23,7 @@ $(call inherit-product, vendor/motorola/ocean/ocean-vendor.mk)
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-aospk
+    $(LOCAL_PATH)/overlay-kraken
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
