2aa46cd3d30fd13ffec0204fdcfd126dfa4d1fad
vulkan-ops <vulkanops@gmail.com>
2021-07-01 16:26:48 -0300

    ocean: Initial derp bringup Signed-off-by: vulkan-ops <vulkanops@gmail.com>

--- device.mk
+++ device.mk
@@ -22,8 +22,7 @@ $(call inherit-product, vendor/motorola/ocean/ocean-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-kraken
+    $(LOCAL_PATH)/overlay
 
 PRODUCT_PACKAGES += \
     NoCutoutOverlay
