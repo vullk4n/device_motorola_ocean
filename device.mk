29afd6f624784650e9975b6822586fa55bf9aa5c
vulkan-ops <vulkanops@gmail.com>
2022-09-23 14:13:46 -0300

    Partial Revert "ocean: bringup for our project" This reverts commit fb8b9e80e85524e70d811fde22fe59024a0a36c0.

--- device.mk
+++ device.mk
@@ -22,7 +22,8 @@ $(call inherit-product, vendor/motorola/ocean/ocean-vendor.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-  $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-lineage
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
 PRODUCT_PACKAGES += \
